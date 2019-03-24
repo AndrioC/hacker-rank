882c5359c22aba43253f57311e920397e185561d
Andrio Corrêa <30298702+AndrioC@users.noreply.github.com>
2019-03-24 18:10:20 -0300

    Update 02-countingvalleys.py

--- 02-countingvalleys.py
+++ 02-countingvalleys.py
@@ -1,53 +1,3 @@
-'''
-Gary is an avid hiker. He tracks his hikes meticulously, paying close attention to small details like topography. During
-his last hike he took exacly n steps. For every step he took, he noted if it was an unphill, U, or a downhill, D step.
-Gary's hikes start and end at sea level and each up or down represents a 1 unit change in altitude. We define the following terms:
-
--> A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
--> A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
-
-Given Gary's sequence of up and down steps during his last hike, find and print the number of valleys he walked through.
-
-For example, if Gary's path is s = [DDUUUUDD], he first enters a valley 2 units deep. The he climbs out an up onto a mountain 2 units high.
-Finally, he returns to sea level and ends his hike.
-
--Function Description-
-Complete the countingValleys function in the editor below. It must return an integer that denotes the number of valleys Gary traversed.
-
-countingValleys has the following parameter(s):
--> n: the number of steps Gary takes
--> s: a string describing his path
-
-
--Input Format-
-The first line contains an integer n, the number of steps in Gary's hike.
-The second line contains a single string s, of n characters that describe his path.
-
--Constraints-
-
--> 2 <= n <= 10^6
--> s[i] ∈ {UD}
-
--Output Format-
-Print a single integer that denotes the number of valleys Gary walked through during his hike.
-
--Sample Input-
-8
-UDDDUDUU
-
--Sample Output-
-1 
-
--Explanation-
-
-If we represent _ as sea level, a step up as /, and a step down as \, Gary's hike can be drawn as:
-
-_/\      _
-   \    /
-    \/\/
-
-'''
-
 #!/bin/python3
 
 import math
@@ -76,8 +26,6 @@ def countingValleys(n, s):
 				valley_number += 1
 
 	return valley_number
-
-
 n = int(input())
 
 s = input()
